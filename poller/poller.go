354b8e36724e5cf86a321c1b1c68dac892802682
Trisome-Bedrock <trisome@bedrock-technology>
2024-07-29 14:48:41 +0800

    poller: wrap syncManagerRewards function

--- poller/poller.go
+++ poller/poller.go
@@ -45,66 +45,74 @@ func (p *Poller) run() {
 			p.Logger.Info("Poller closed")
 			return
 		case <-t.C:
-			// 1. Query datum
-			rewards, err := p.IOTXStakingCaller.GetManagerReward(nil)
-			if err != nil {
-				p.Logger.Error("failed to query manger rewards", err)
+			if err := p.syncManagerRewards(); err != nil {
 				continue
-			}
-
-			ratio, err := p.IOTXStakingCaller.ExchangeRatio(nil)
-			if err != nil {
-				p.Logger.Error("failed to query exchange ratio", err)
-				continue
-			}
-
-			date, year, month, day := utils.Date(time.Now())
-			uniIOTXRewards := utils.UniIOTXRewards(rewards, ratio)
-			latestData := &common.DailyManagerRewards{
-				Date:           date,
-				Year:           year,
-				Month:          month,
-				Day:            day,
-				IOTXRewards:    rewards.String(),
-				UniIOTXRewards: uniIOTXRewards.String(),
-				ExchangeRatio:  ratio.String(),
-			}
-
-			// 2. Update storage
-			err = p.Storer.CreateOrUpdateDailyManagerRewards(latestData, false)
-			if err != nil {
-				p.Logger.Error("failed to store manager rewards", err)
-				continue
-			}
-
-			// 3. Update cache
-			var oldData *common.DailyManagerRewards
-			if val, ok := p.Cacher.Get(common.CacheKeyLatestManagerRewards); ok {
-				oldData = val.(*common.DailyManagerRewards)
-			}
-
-			p.Cacher.SetWithTTL(common.CacheKeyLatestManagerRewards, latestData, 1, ttl)
-			p.Cacher.Wait()
-
-			// 4. Update metrics
-			rewardsVal := utils.BigIntToFloat64(rewards, 1e18, 3)
-			uniIOTXRewardsVal := utils.BigIntToFloat64(uniIOTXRewards, 1e18, 3)
-			ratioVal := utils.BigIntToFloat64(ratio, 1e18, 3)
-			if oldData == nil {
-				metrics.ManagerRewards.Set(rewardsVal)
-				metrics.UniIOTXManagerRewards.Set(uniIOTXRewardsVal)
-				metrics.ExchangeRatio.Set(ratioVal)
-			} else {
-				if latestData.IOTXRewards != oldData.IOTXRewards {
-					metrics.ManagerRewards.Set(rewardsVal)
-				}
-				if latestData.UniIOTXRewards != oldData.UniIOTXRewards {
-					metrics.UniIOTXManagerRewards.Set(uniIOTXRewardsVal)
-				}
-				if latestData.ExchangeRatio != oldData.ExchangeRatio {
-					metrics.ExchangeRatio.Set(ratioVal)
-				}
 			}
 		}
 	}
+}
+
+func (p *Poller) syncManagerRewards() error {
+	// 1. Query datum
+	rewards, err := p.IOTXStakingCaller.GetManagerReward(nil)
+	if err != nil {
+		p.Logger.Error("failed to query manger rewards", err)
+		return err
+	}
+
+	ratio, err := p.IOTXStakingCaller.ExchangeRatio(nil)
+	if err != nil {
+		p.Logger.Error("failed to query exchange ratio", err)
+		return err
+	}
+
+	date, year, month, day := utils.Date(time.Now())
+	uniIOTXRewards := utils.UniIOTXRewards(rewards, ratio)
+	latestData := &common.DailyManagerRewards{
+		Date:           date,
+		Year:           year,
+		Month:          month,
+		Day:            day,
+		IOTXRewards:    rewards.String(),
+		UniIOTXRewards: uniIOTXRewards.String(),
+		ExchangeRatio:  ratio.String(),
+	}
+
+	// 2. Update storage
+	err = p.Storer.CreateOrUpdateDailyManagerRewards(latestData, false)
+	if err != nil {
+		p.Logger.Error("failed to store manager rewards", err)
+		return err
+	}
+
+	// 3. Update cache
+	var oldData *common.DailyManagerRewards
+	if val, ok := p.Cacher.Get(common.CacheKeyLatestManagerRewards); ok {
+		oldData = val.(*common.DailyManagerRewards)
+	}
+
+	p.Cacher.SetWithTTL(common.CacheKeyLatestManagerRewards, latestData, 1, ttl)
+	p.Cacher.Wait()
+
+	// 4. Update metrics
+	rewardsVal := utils.BigIntToFloat64(rewards, 1e18, 3)
+	uniIOTXRewardsVal := utils.BigIntToFloat64(uniIOTXRewards, 1e18, 3)
+	ratioVal := utils.BigIntToFloat64(ratio, 1e18, 3)
+	if oldData == nil {
+		metrics.ManagerRewards.Set(rewardsVal)
+		metrics.UniIOTXManagerRewards.Set(uniIOTXRewardsVal)
+		metrics.ExchangeRatio.Set(ratioVal)
+	} else {
+		if latestData.IOTXRewards != oldData.IOTXRewards {
+			metrics.ManagerRewards.Set(rewardsVal)
+		}
+		if latestData.UniIOTXRewards != oldData.UniIOTXRewards {
+			metrics.UniIOTXManagerRewards.Set(uniIOTXRewardsVal)
+		}
+		if latestData.ExchangeRatio != oldData.ExchangeRatio {
+			metrics.ExchangeRatio.Set(ratioVal)
+		}
+	}
+
+	return nil
 }
